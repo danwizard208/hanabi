23cad6edd8b8bfba6fedfc7b1cb54692eaabcb33
Daniel Asher Resnick <danwizard208@gmail.com>
2016-02-07 23:58:28 -0600

    Add Model namespace and Deck class

---
+++ Hanabi/Model/Deck.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Hanabi.Model
+{
+    public class Deck
+    {
+        public static Deck CreateDeck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CardsRemaining { get; set; }
+    }
+}
