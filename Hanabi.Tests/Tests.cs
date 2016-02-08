720d2aed0ecd7112e7044b678ca0daa5b50ef426
Daniel Asher Resnick <danwizard208@gmail.com>
2016-02-07 23:58:01 -0600

    Add create deck test

--- Hanabi.Tests/Tests.cs
+++ Hanabi.Tests/Tests.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using Should;
+using Hanabi;
+using Hanabi.Model;
 
 namespace Hanabi.Tests
 {
@@ -10,6 +12,12 @@ namespace Hanabi.Tests
         public void Should_be_true()
         {
             true.ShouldBeTrue();
+        }
+
+        [Test]
+        public void Should_Make_50_Card_Deck()
+        {
+            Deck.CreateDeck().CardsRemaining.ShouldEqual(50);
         }
     }
 }
